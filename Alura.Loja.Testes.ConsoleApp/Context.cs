4669e90fdfd5e37f123c1b259183b8b84d1f58bc
HackbartPR <cgharckbart@gmail.com>
2023-07-08 18:44:14 -0300

    Criando Context e Salvando Primeiros Dados

---
+++ Alura.Loja.Testes.ConsoleApp/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Context : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=lojadb;Trusted_Connection=true;");
+        }
+    }
+}
